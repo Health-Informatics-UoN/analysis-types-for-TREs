5efff3dea15f731fca01581af2b519578297f89f
James Mitchell-White <jamesjimitchell@gmail.com>
2026-02-16 13:02:40 +0000

    working examples

--- src/components/aggregateFunctions.ts
+++ src/components/aggregateFunctions.ts
@@ -1,18 +1,84 @@
-export enum FunctionDescriptor {
-  Sum,
-  Product,
-  Count,
-  SumOfSquares,
-};
+export interface InnerFunction<T, S> {
+  // apply is what happens to each row
+  apply: (item: T) => S;
+  // merge is how rows within a 
+  merge: (a: S, b: S) => S;
+  identity: S;
+}
 
-export type AggregateFunction = {
-  innerFunction: Function;
-  innerFunctionDescriptors: String[];
-  outerFunction: Function;
-  outerFunctionDescriptors: String[];
-};
+export function runInner<T, S>(
+  inner: InnerFunction<T, S>,
+  data: T[][]
+): S[] {
+  return data
+  .map(
+    ds => ds
+    .map(inner.apply)
+    .reduce(inner.merge, inner.identity)
+  )
+}
 
-export function applyAggregate (aggregateFunction: AggregateFunction, data: number[][]): number {
-  return data.map(aggregateFunction.innerFunction)
-             .reduce(aggregateFunction.outerFunction)
+export interface OuterFunction<S, R> {
+  // the aggregate function is what takes the intermediate results and produces the final result
+  aggregate: (aggregate: S, intermediate: S) => S;
+  identity: S;
+  finalise: (aggregate: S) => R;
+}
+
+export interface Decomposable<T, S, R> {
+  inner: InnerFunction<T, S>;
+  outer: OuterFunction<S, R>;
+}
+
+export function computeAggregate<T, S, R>(
+  agg: Decomposable<T, S, R>,
+  data: T[][],
+): R {
+    const intermediates: S[] = data
+    .map(
+      ds => ds
+      .map(agg.inner.apply)
+      .reduce(agg.inner.merge, agg.inner.identity)
+    );
+
+    const aggregateResult = intermediates.reduce(
+      agg.outer.aggregate,
+      agg.outer.identity
+    )
+
+    return agg.outer.finalise(aggregateResult)
+}
+
+// Inner Functions
+
+export const countRows: InnerFunction<number, number> = {
+    apply: (_) => 1,
+    merge: (a, b) => a + b,
+    identity: 0
+}
+
+export const sumRows: InnerFunction<number, number> = {
+  apply: (x) => x,
+  merge: (a, b) => a + b,
+  identity: 0
+}
+
+export const sumAndCountRows: InnerFunction<number, {sum: number, count: number}> = {
+  apply: (x) => ({sum: x, count: 1}),
+  merge: (a, b) => ({sum: a.sum + b.sum, count: a.count + b.count}),
+  identity: {sum: 0, count: 0}
+}
+
+// Outer Functions
+
+export const sumIntermediates: OuterFunction<number, number> = {
+    aggregate: (a, b) => a + b,
+    identity: 0,
+    finalise: (x) => x
+}
+
+export const gatherAvgIntermediates: OuterFunction<{sum: number, count: number}, number> = {
+  aggregate: (a, b) => ({sum: a.sum + b.sum, count: a.count + b.count}),
+  identity: {sum: 0, count: 0},
+  finalise: (x: {sum: number, count: number}) => x.sum/x.count
 }
--- src/decomposable-analysis.md
+++ src/decomposable-analysis.md
@@ -9,5 +9,54 @@ Generally, a decomposable analysis consists of two parts: some function on the d
 If you want to carry out basic statistics, you can get a lot by combining a few of these functions.
 
 ```js
-import {AggregateFunction} from "./components/aggregateFunctions.ts"
+import {
+    InnerFunction,
+    runInner,
+    OuterFunction,
+    Decomposable,
+    computeAggregate,
+    countRows,
+    sumRows,
+    sumIntermediates
+} from "./components/aggregateFunctions.js"
+```
+
+```js
+const dummyDataInput = view(Inputs.textarea(
+  {
+    label: "Put some data in here!",
+    value: `[
+      [1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10],
+      [11, 12, 13, 14, 15]
+    ]`,
+    submit: true
+  }
+))
+```
+
+```js
+const dummyData = JSON.parse(dummyDataInput);
+```
+
+```js
+runInner(countRows, dummyData)
+```
+
+```ts echo
+const countAll: Decomposable<number, number, number> = {
+    inner: countRows,
+    outer: sumIntermediates
+}
+
+display(computeAggregate(countAll, dummyData))
+```
+
+```ts echo
+const sumAll: Decomposable<number, number, number> = {
+    inner: sumRows,
+    outer: sumIntermediates
+}
+
+display(computeAggregate(sumAll, dummyData))
 ```
