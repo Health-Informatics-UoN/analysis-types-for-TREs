58c1a7b4ab4b7b28f4b814d83000ea10090ab30b
James Mitchell-White <jamesjimitchell@gmail.com>
2026-02-16 13:31:26 +0000

    add more analysis descriptions

--- src/components/decomposables.ts
+++ src/components/decomposables.ts
@@ -6,10 +6,26 @@ import { sumIntermediates, gatherAvgIntermediates, outerFunctionDescriptions } f
 export const analyses: AnalysisDescription<any, any, number>[]  = [
   {
     label: "Count All",
-    decomposableDescription: "Count the rows across the dataset",
+    decomposableDescription: "Count the rows across the datasets",
     inner: countRows,
     outer: sumIntermediates,
     innerDescription: innerFunctionDescriptions.countRows,
     outerDescription: outerFunctionDescriptions.sumIntermediates
+  },
+  {
+    label: "Sum All",
+    decomposableDescription: "Sum values across the datasets",
+    inner: sumRows,
+    outer: sumIntermediates,
+    innerDescription: innerFunctionDescriptions.sumRows,
+    outerDescription: outerFunctionDescriptions.sumIntermediates
+  },
+  {
+    label: "Mean across all",
+    decomposableDescription: "Calculate the mean of all values across datasets",
+    inner: sumAndCountRows,
+    outer: gatherAvgIntermediates,
+    innerDescription: innerFunctionDescriptions.sumAndCountRows,
+    outerDescription: outerFunctionDescriptions.gatherAvgIntermediates
   }
-}
+]
